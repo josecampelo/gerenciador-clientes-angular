4991ed03e4bbd0ed1d2d1d0f970ac1b094adbcf7
José Campelo <josecampelojr@gmail.com>
2026-01-04 17:07:07 -0300

    feat: Implementação do formulário de cadastro de clientes

--- src/app/components/cadastro-cliente/cadastro-cliente.component.ts
+++ src/app/components/cadastro-cliente/cadastro-cliente.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-cadastro-cliente',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    NgxMaskDirective
+  ],
   templateUrl: './cadastro-cliente.component.html',
   styleUrl: './cadastro-cliente.component.scss'
 })
 export class CadastroClienteComponent {
+  private fb = inject(FormBuilder);
 
+  cadastroForm: FormGroup = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [Validators.required]],
+      cpf: ['', [Validators.required]]
+  });
+
+  salvar(): void {
+    if (this.cadastroForm.valid) {
+      console.log('Dados do Cliente:', this.cadastroForm.value);
+    }
+  }
 }
