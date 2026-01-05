6c9bca9f77e6b38d9f32c3c6ba31c427d665ae55
José Campelo <josecampelojr@gmail.com>
2026-01-05 06:35:52 -0300

    feat: Configuração do roteamento e criação do componente de consulta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CadastroClienteComponent } from './components/cadastro-cliente/cadastro-cliente.component';
+import { ConsultaClienteComponent } from './components/consulta-cliente/consulta-cliente.component';
 
 export const routes: Routes = [
-    {path: 'cadastrar', component: CadastroClienteComponent}
+    {path: 'cadastrar', component: CadastroClienteComponent},
+    { path: 'consultar', component: ConsultaClienteComponent }
 ];
---
+++ src/app/components/consulta-cliente/consulta-cliente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-cliente',
+  imports: [],
+  templateUrl: './consulta-cliente.component.html',
+  styleUrl: './consulta-cliente.component.scss'
+})
+export class ConsultaClienteComponent {
+
+}
